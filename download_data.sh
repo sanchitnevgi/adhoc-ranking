45ecdbb3db0869388e2230cbce4227b82e8a986d
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-11-03 09:45:22 -0700

    Make data download parallel with xargs

--- download_data.sh
+++ download_data.sh
@@ -1,23 +1,10 @@
+#!/bin/bash
+
 # Create data folder
 mkdir -p ./data
 
 cd data
-# Download Corpus
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.trec.gz
-# Download document lookup
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs-lookup.tsv.gz
+# Paralleize the data download, 4 processes
+cat ../data_files.txt | xargs -n 1 -P 4 wget
 
-# Train data
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz
-
-# Dev data
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docdev-queries.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docdev-top100.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docdev-qrels.tsv.gz
-
-# Test data
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-test2019-queries.tsv.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctest2019-top100.gz
-wget https://trec.nist.gov/data/deep/2019qrels-docs.txt
+cd ../
